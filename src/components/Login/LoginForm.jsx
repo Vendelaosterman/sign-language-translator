c970cb01de2bbacb20dcf8cb7e8dfbc93754d760
levenfalk <nathalie.levenfalk@se.experis.com>
2023-07-14 11:58:26 +0200

    feat: setup app and user contexts

--- src/components/Login/LoginForm.jsx
+++ src/components/Login/LoginForm.jsx
@@ -1,7 +1,9 @@
-import{ useForm } from 'react-hook-form'
-import {useState} from 'react'
-import { loginUser } from '../../api/user'
-import { storageSave } from '../../utils/storage'
+import{ useForm } from 'react-hook-form';
+import { useState, useEffect } from 'react';
+import { loginUser } from '../../api/user';
+import { storageSave } from '../../utils/storage';
+import { useNavigate } from "react-router-dom";
+import  { useUser } from "../../context/UserContext";
 
 const usernameConfig = {
     required: true,
@@ -9,28 +11,35 @@ const usernameConfig = {
 }
 
 const LoginForm = () => {
+    const { register, handleSubmit, formState: { errors } } = useForm()
+    const { user, setUser } = useUser()
+    const navigate = useNavigate()
 
-    const {
-        register,
-        handleSubmit, 
-        formState: { errors }
-    } = useForm()
-
-    const [loading, setLoading] = useState(false)
+    // Local State
+    const [ loading, setLoading ] = useState(false)
     const [ apiError, setApiError ] = useState(null)
 
+    // Side Effects
+    useEffect(() => {
+        if (user !== null) {
+            navigate('/profile')
+        }
+    }, [ user, navigate ]) // If empty Deps - Only run 1ce
+
+    // Event Handlers
     const onSubmit = async ({username}) => {
         setLoading(true)
-        const [error, user] = await loginUser(username)
+        const [error, userResponse] = await loginUser(username)
         if (error !== null) {
             setApiError(error)
         }
-        if (user !== null) {
-            storageSave('translator-user', user)
+        if (userResponse !== null) {
+            storageSave('translator-user', userResponse)
+            setUser(userResponse)
         }
         setLoading(false)
     }
-
+    // Render Functions
     const errorMessage = (() => {
         if(!errors.username){
             return null
