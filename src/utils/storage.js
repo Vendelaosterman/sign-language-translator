1a99394475050b4bebe9312f151a2baa341b7624
levenfalk <nathalie.levenfalk@se.experis.com>
2023-07-18 15:37:03 +0200

    feat: store user in sessionStorage

--- src/utils/storage.js
+++ src/utils/storage.js
@@ -1,9 +1,9 @@
 export const storageSave = (key, value) => {
-    localStorage.setItem(key, JSON.stringify(value))
+    sessionStorage.setItem(key, JSON.stringify(value))
 }
 
 export const storageRead = key => {
-    const data = localStorage.getItem(key)
+    const data = sessionStorage.getItem(key)
     if(data) {
         console.log("test")
         return JSON.parse(data)
@@ -13,5 +13,5 @@ export const storageRead = key => {
 }
 
 export const storageDelete = key => {
-    localStorage.removeItem(key)
+    sessionStorage.removeItem(key)
 }
